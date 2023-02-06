5be405d4cad485cdbe3c38dc88bd53d49c2e1c88
Alexander Zwisler <alex-z-@web.de>
2023-02-06 22:28:01 +0100

    fix: dont set base url

--- editor/src/main.ts
+++ editor/src/main.ts
@@ -1,9 +1,6 @@
-import { OpenAPI } from '../openapi-typescript-codegen';
 import './component/available-nodes.component';
 import './component/toolbar.component';
 import './component/editor.component';
 import './component/cursor-context-menu.component';
 import './component/node-details.component';
 import './component/notification.component';
-
-OpenAPI.BASE = 'http://localhost:3000';
